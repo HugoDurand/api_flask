72aaeddd2d85f1b4b964bcfb87bc73b384fd8438
Hugo DURAND <hugosmails94@gmail.com>
2019-06-01 19:26:01 +0200

    Configure travis ci (#3)
    * :pencil: add travis file and test ci
    
    * fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! :pencil: add travis file and test ci
    
    * update gitignore

--- app/main/config.py
+++ app/main/config.py
@@ -5,14 +5,17 @@ postgres_local_base = os.environ['DATABASE_URL']
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
     DEBUG = False
+
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = postgres_local_base
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class TestingConfig(Config):
     DEBUG = True
@@ -21,9 +24,11 @@ class TestingConfig(Config):
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = postgres_local_base
+
 
 config_by_name = dict(
     dev=DevelopmentConfig,
