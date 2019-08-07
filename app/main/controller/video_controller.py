c94acf571e825e802c18bbb6fc9a2e8488a104fe
Hugo DURAND <hugosmails94@gmail.com>
2019-08-07 18:33:42 +0200

    add video model service and controller to the api (#16)

---
+++ app/main/controller/video_controller.py
@@ -0,0 +1,38 @@
+from flask import request
+from flask_restplus import Resource
+
+from ..util.dto import VideoDto
+from ..service.video_service import new_video, get_videos, get_video
+from ..util.decorator import token_required
+
+api  = VideoDto.api
+_video = VideoDto.video
+
+
+@api.route('/')
+class getPostVideo(Resource):
+
+    @api.doc('list all videos')
+    @api.marshal_list_with(_video, envelope='data')
+    def get(self):
+        return get_videos()
+    
+    @api.response(201, 'Video successfully created.')
+    @api.doc('create a new video')
+    @api.expect(_video, validate=True)
+    def post(self):
+        data = request.json
+        return new_video(data=data)
+
+@api.route('/<id>')
+@api.param('id', 'video identifier')
+class getVideoById(Resource):
+
+    @api.doc('get a video')
+    @api.marshal_list_with(_video)
+    def get(self, id):
+        video = get_video(id)
+        if not video:
+            api.abort(404)
+        else:
+            return video
---
+++ app/main/model/video.py
@@ -0,0 +1,15 @@
+from .. import db
+import datetime
+
+class Video(db.Model):
+    """ Video Model """
+    __tablename__ = 'video'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(255), nullable=False)
+    link = db.Column(db.String(500), nullable=False)
+    duration = db.Column(db.Time, nullable=False)
+    post_date = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self):
+        self.post_date = datetime.datetime.now()
