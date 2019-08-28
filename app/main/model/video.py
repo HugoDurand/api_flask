e750e3560b0215f106a5d4a8b3c686d1ad3a9318
Hugo Durand <hugosmails94@gmail.com>
2019-08-28 15:52:55 +0200

    time in float

--- app/main/model/video.py
+++ app/main/model/video.py
@@ -8,8 +8,7 @@ class Video(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String(255), nullable=False)
     link = db.Column(db.String(500), nullable=False)
-    duration = db.Column(db.String(255), nullable=False)
-    #duration = db.Column(db.Time, nullable=False)
+    duration = db.Column(db.Float, nullable=False)
     post_date = db.Column(db.DateTime, nullable=False)
     comments = db.relationship(Comment, lazy="dynamic", cascade='all, delete-orphan', passive_deletes=True)
     
