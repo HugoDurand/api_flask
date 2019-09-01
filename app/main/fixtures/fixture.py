ce240bb9f2e73cd57a72bac6a4e7e1494209571b
Hugo Durand <hugosmails94@gmail.com>
2019-09-01 14:15:45 +0200

    creating data seeders for all json data files

--- app/main/fixtures/fixture.py
+++ app/main/fixtures/fixture.py
@@ -1,8 +1,22 @@
-import json
-from pprint import pprint
+import json, os
+from app.main import db
 
 
 def get_json():
-    with open('app/main/fixtures/fixtures.json') as f:
-        data = json.load(f)
-        pprint(data)
+    path_to_json = 'app/main/fixtures/'
+    json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
+    for j in json_files:
+        with open('app/main/fixtures/'+j) as file:
+            data = json.load(file)
+        return sort_data(data)
+
+
+def sort_data(data):
+    # TODO: Avois nested loops
+    for index, d in enumerate(data['data']):
+        for key, value in data['data'][index].items():
+            model = data['model'](
+                key=value,
+            )
+            db.session.add(model)
+            db.session.commit()
