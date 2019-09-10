493904c3367cfd33d395b10c86f18ead1299ca76
hugosmails94 <hugosmails94@gmail.com>
2019-09-10 16:05:12 +0200

    remove graphene interfaces relay node

--- app/main/graphql/query.py
+++ app/main/graphql/query.py
@@ -1,13 +1,27 @@
 import graphene
-from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 from .schema import VideoObject, CommentObject, UserObject, CategoryObject
 from .mutations import Mutation
+from ..model.video import Video
+from ..model.comment import Comment
+from ..model.user import User
+from ..model.category import Category
 
 class Query(graphene.ObjectType):
-    node = graphene.relay.Node.Field()
-    videos = SQLAlchemyConnectionField(VideoObject)
-    comments = SQLAlchemyConnectionField(CommentObject)
-    users = SQLAlchemyConnectionField(UserObject)
-    categories = SQLAlchemyConnectionField(CategoryObject)
+    videos = graphene.List(VideoObject)
+    comments = graphene.List(CommentObject)
+    users = graphene.List(UserObject)
+    categories = graphene.List(CategoryObject)
+
+    def resolve_videos(self, info, **kwargs):
+        return Video.query.all()
+    
+    def resolve_comments(self, info, **kwargs):
+        return Comment.query.all()
+
+    def resolve_users(self, info, **kwargs):
+        return User.query.all()
+
+    def resolve_categories(self, info, **kwargs):
+        return Category.query.all()
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
--- app/main/graphql/schema.py
+++ app/main/graphql/schema.py
@@ -9,22 +9,18 @@ from graphene_sqlalchemy import SQLAlchemyObjectType
 class VideoObject(SQLAlchemyObjectType):
     class Meta:
         model = Video
-        interfaces = (graphene.relay.Node, )
 
 
 class CommentObject(SQLAlchemyObjectType):
     class Meta:
         model = Comment
-        interfaces = (graphene.relay.Node, )
 
 
 class CategoryObject(SQLAlchemyObjectType):
     class Meta:
         model = Category
-        interfaces = (graphene.relay.Node, )
 
 
 class UserObject(SQLAlchemyObjectType):
     class Meta:
         model = User
-        interfaces = (graphene.relay.Node, )
