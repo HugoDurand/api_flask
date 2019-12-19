a50ed5e6a30f4b29934a7f2f06777ac05f673ad6
Hugo DURAND <hugosmails94@gmail.com>
2019-12-19 16:05:37 +0100

    add mixins to search through model from controller (#28)
    create elastic service and add mixin to call search function from model in controller
    
    fixup! create elastic service and add mixin to call search function from model in controller

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from .main.controller.user_controller import api as user_ns
 from .main.controller.video_controller import api as video_ns
 from .main.controller.comment_controller import api as comment_ns
 from .main.controller.category_controller import api as category_ns
+from .main.controller.elastic_controller import api as elastic_ns
 
 blueprint = Blueprint('api', __name__)
 
@@ -20,3 +21,4 @@ api.add_namespace(user_ns, path='/user')
 api.add_namespace(video_ns, path='/video')
 api.add_namespace(comment_ns, path='/comment')
 api.add_namespace(category_ns, path='/category')
+api.add_namespace(elastic_ns, path='/elastic')
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from elasticsearch import Elasticsearch
 
 from .config import config_by_name
 
 db = SQLAlchemy()
 flask_bcrypt = Bcrypt()
-
+es = Elasticsearch('http://elasticsearch:9200')
 
 def create_app(config_name):
     app = Flask(__name__)
