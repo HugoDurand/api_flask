d87ab04b8b9f14a4aa1a30beaaea705f57d7e2e6
@hugo.durand <hugo.durand@radiofrance.com>
2019-12-19 16:28:25 +0100

    add basic monitoring dashboard configuration

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+import flask_monitoringdashboard as dashboard
 from elasticsearch import Elasticsearch
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
 
 from .config import config_by_name
 
@@ -9,10 +10,13 @@ db = SQLAlchemy()
 flask_bcrypt = Bcrypt()
 es = Elasticsearch('http://elasticsearch:9200')
 
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
     flask_bcrypt.init_app(app)
+    dashboard.bind(app)
+    dashboard.config.init_from(file="../../config.cfg")
 
     return app
