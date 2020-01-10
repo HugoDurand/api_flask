702c4fbc9b5a487cd4407301b1039a2f16c3fbf3
@hugo.durand <hugo.durand@radiofrance.com>
2020-01-10 17:31:44 +0100

    remove controllers, dto and namespaces
    fixup! remove controllers, dto and namespaces

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,3 @@
-from flask_restplus import Api
 from flask import Blueprint
 
-from .main.controller.auth_controller import api as auth_ns
-from .main.controller.user_controller import api as user_ns
-from .main.controller.video_controller import api as video_ns
-from .main.controller.comment_controller import api as comment_ns
-from .main.controller.category_controller import api as category_ns
-from .main.controller.elastic_controller import api as elastic_ns
-
 blueprint = Blueprint('api', __name__)
-
-api = Api(blueprint,
-          title='FLASK API',
-          version='1.0',
-          description='Flask Api Documentation'
-          )
-
-api.add_namespace(auth_ns)
-api.add_namespace(user_ns, path='/user')
-api.add_namespace(video_ns, path='/video')
-api.add_namespace(comment_ns, path='/comment')
-api.add_namespace(category_ns, path='/category')
-api.add_namespace(elastic_ns, path='/elastic')
